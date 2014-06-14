ccf4ac74972b189220d9894c4e32a7d063fbba6d
Gregg Tavares <github@greggman.com>
2014-06-14 16:42:35 +0900

    Initial commit
    The basics are there. Each command lives in `lib/cmds/<cmdname>.js`
    They are loaded on the fly.
    
    Other than parsing args actual implementation should live in happyFunTimes

---
+++ bin/hft.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+;(function () { // wrapper in case we're in module_context mode
+
+"use strict";
+
+process.title = "hft";
+
+var path = require('path');
+var fs = require('fs');
+var args = require('minimist')(process.argv.slice(2));
+
+var cmd = args._[0];
+if (!cmd) {
+  printUsage();
+  process.exit(1);
+} else {
+  var cmdPath = path.join(__dirname, "../lib/cmds", cmd + ".js");
+  if (!fs.existsSync(cmdPath)) {
+    console.error("unknown cmd: " + cmd);
+    printUsage();
+    process.exit(1);
+  }
+  var cmdModule = require('../lib/cmds/' + cmd);
+  cmdModule.cmd(args);
+}
+
+function printUsage() {
+  var usage = [];
+  var cmds = fs.readdirSync(path.join(__dirname, "../lib/cmds"));
+  cmds.forEach(function(cmd) {
+    if (cmd.substr(-3) != ".js") {
+      return;
+    }
+
+    var cmdUsage = require('../lib/cmds/' + cmd).usage;
+    usage.push(cmd.substring(0, cmd.length - 3) + ":\n    " + cmdUsage.split("\n").join("\n    ") + "\n");
+  });
+  console.log("usage: hft cmd [options]\n\n" + usage.join("\n"));
+};
+
+
+}());
+
